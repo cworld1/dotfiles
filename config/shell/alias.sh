0a910046708ac934fe44112c70d0eb7c3753af04
CWorld <cworld0@qq.com>
2024-01-07 00:41:06 +0800

    chore(shell): :rocket: update alias

--- config/shell/alias.sh
+++ config/shell/alias.sh
@@ -8,18 +8,17 @@ setalias mkdir 'mkdir -v -p'
 # Pacman
 setalias paci 'sudo pacman -S'
 setalias pacr 'sudo pacman -R'
-setalias pacu 'sudo pacman -Syyu'
+setalias pacu 'sudo pacman -Syu'
 setalias wget "wget --hsts-file=\"$XDG_DATA_HOME/wget-hsts\""
 
 # User
 setalias py python
 setalias jo joshuto
 setalias osfetch neofetch
-setalias files nautilus
-# setalias rg ripgrep
-setalias clean~ xdg-ninja
+setalias files explorer
+# setalias clean~ xdg-ninja
 # Clipboard
-setalias clip 'cliphist list | fzf | cliphist decode | wl-copy'
+# setalias clip 'cliphist list | fzf | cliphist decode | wl-copy'
 # Git
 setalias gitfetch onefetch
 setalias gitc 'git clone'
@@ -33,12 +32,12 @@ setalias nvi nvim
 # Paru
 setalias parui 'paru -S'
 setalias parur 'paru -R'
-setalias paruu 'paru -Syyu'
+setalias paruu 'paru -Syu'
 setalias paruc 'paru -c'
 # Cmake
 setalias cmc 'cmake . -G "Unix Makefiles" -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1'
 setalias cmr 'cmake --build build && build/main'
 # Clash
-setalias clashr 'systemctl --user restart clash-meta.service'
-setalias clashs 'systemctl --user status clash-meta.service'
-setalias clashd 'firefox "http://127.0.0.1:9090/ui/#/"'
+# setalias clashr 'systemctl --user restart clash-meta.service'
+# setalias clashs 'systemctl --user status clash-meta.service'
+# setalias clashd 'firefox "http://127.0.0.1:9090/ui/#/"'
