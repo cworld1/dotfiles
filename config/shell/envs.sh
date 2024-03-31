e4ece642bc7a4f18d50c752b92d7d64cf0baa166
CWorld <cworld0@qq.com>
2024-03-31 21:02:00 +0800

    feat(shell): :rocket: add redirect support for less. ltdr and node

--- config/shell/envs.sh
+++ config/shell/envs.sh
@@ -10,20 +10,24 @@ setenv HOMEBREW_PIP_INDEX_URL "https://mirrors.bfsu.edu.cn/pypi/web/simple"
 setenv LANG en_US.UTF-8
 setenv TERM xterm-256color
 setenv EDITOR nvim
+
 # Prevent trashes on the $HOME directory
 setenv XDG_RUNTIME_DIR /run/user/$UID
 setenv XDG_CACHE_HOME $HOME/.cache
 setenv XDG_CONFIG_HOME $HOME/.config
 setenv XDG_DATA_HOME $HOME/.local/share
 setenv XDG_STATE_HOME $HOME/.local/state
-
 # Bash
 setenv HISTFILE "$XDG_STATE_HOME"/bash/history
-# Python
-setenv PYTHONSTARTUP "/etc/python/pythonrc"
+# Less
+setenv LESSHISTFILE "$XDG_STATE_HOME"/less/history
+# Node
+setenv NODE_REPL_HISTORY "$XDG_DATA_HOME"/node_repl_history
 # NPM
 setenv NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME"/npm/npmrc
 # PNPM
 setenv PNPM_HOME "$XDG_DATA_HOME"/pnpm
 # Rust (cargo)
 setenv CARGO_HOME "$XDG_DATA_HOME"/cargo
+# Tldr
+setenv TLDR_CACHE_DIR "$XDG_CACHE_HOME"/tldr
