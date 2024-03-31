4414a446d771965465486bb5999a10668dd88da4
CWorld <cworld0@qq.com>
2024-03-31 20:57:13 +0800

    feat(fish): :zap: add prompt demo; add yazi and fzf completions

--- config/fish/customize.fish
+++ config/fish/customize.fish
@@ -1,12 +1,21 @@
 # Prompt frefix
-set prompt_prefix "󰀵" # command logo
+set prompt_prefix "" # command logo
+# "": macOS     "": Windows  "": Android
+# "": Archlinux "": Fedora   "": Ubuntu
+# "": centOS    "": OpenSUSE
 
 # Proxy
 set hostip "127.0.0.1"
 # set hostip $(cat /etc/resolv.conf | grep -oP '(?<=nameserver\ ).*') # for wsl
 
-# Yazi: to change the current working directory when exiting Yazi.
-function ya
+# Zoxide: quick path jump tool
+zoxide init fish | source
+
+# Fzf integration
+fzf --fish | source
+
+# Yazi
+function yy # to change the current working directory when exiting yazi
     set tmp (mktemp -t "yazi-cwd.XXXXX")
     yazi $argv --cwd-file="$tmp"
     if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
@@ -14,9 +23,12 @@ function ya
     end
     rm -f -- "$tmp"
 end
-
-# Zoxide: quick path jump tool
-zoxide init fish | source
+complete -c yazi -l cwd-file -d 'Write the cwd on exit to this file' -r -F
+complete -c yazi -l chooser-file -d 'Write the selected files on open emitted by the chooser mode' -r -F
+complete -c yazi -l clear-cache -d 'Clear the cache directory'
+complete -c yazi -l debug -d 'Print debug information'
+complete -c yazi -s V -l version -d 'Print version'
+complete -c yazi -s h -l help -d 'Print help'
 
 # Venv auto actiavation
 function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
