309b7c2c471d0c4c3af70254bd22c1761716ce94
CWorld <cworld0@qq.com>
2024-06-16 00:19:57 +0800

    feat: :sparkles: fix rust error if installation way is diffrent

--- config/fish/confs/rust.fish
+++ config/fish/confs/rust.fish
@@ -2,4 +2,6 @@
 set -gx CARGO_HOME "$XDG_DATA_HOME"/cargo
 set -gx RUSTUP_HOME "$XDG_DATA_HOME"/rustup
 
-source "$XDG_DATA_HOME"/cargo/env.fish
+
+if [ -f "$XDG_DATA_HOME"/cargo/env.fish ]
+    source "$XDG_DATA_HOME"/cargo/env.fish
