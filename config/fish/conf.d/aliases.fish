08b1ea0b8be74a76cf1324b0e9c531c81899ae64
CWorld <cworld0@qq.com>
2025-12-28 10:58:47 +0800

    feat(fish): :sparkles: enhance aliases for improved directory management

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -3,9 +3,12 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
 # System
-alias where whereis
-alias rmf 'rm -rf'
+alias rmf 'rm -rf' # force delete
+alias tempcd 'mktemp -d | cd' # make temp dir & cd
 alias mkdir 'mkdir -v -p'
+function mkcd -d "Create and enter directory"
+    mkdir -p $argv && cd $argv
+end
 alias wget 'wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
 
 # Shell
