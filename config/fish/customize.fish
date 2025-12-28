dbe2bdd20777450a7337c834fedf0f29c1dcec3b
CWorld <cworld0@qq.com>
2025-12-28 11:30:34 +0800

    style(fish): format the code

--- config/fish/customize.fish
+++ config/fish/customize.fish
@@ -38,39 +38,39 @@ source $fish_confs/nvim.fish
 
 # [Others] sys detect
 switch (uname)
-    case Linux
-        set -x OSTYPE linux
+  case Linux
+    set -x OSTYPE linux
 alias copy xclip
-        alias trash 'gio trash'
-    case Darwin
-        set -x OSTYPE macos
+    alias trash 'gio trash'
+  case Darwin
+    set -x OSTYPE macos
 alias copy pbcopy
-        function trash -d "Move files to trash on macOS"
-            for arg in $argv
-                set -l file (realpath "$arg")
-                /usr/bin/osascript -e "tell application \"Finder\" to delete POSIX file \"$file\"" >/dev/null
-            end
-        end
-    case 'MSYS_NT*'
-        set -x OSTYPE windows
-        alias open explorer
+    function trash -d "Move files to trash on macOS"
+      for arg in $argv
+        set -l file (realpath "$arg")
+        /usr/bin/osascript -e "tell application \"Finder\" to delete POSIX file \"$file\"" >/dev/null
+      end
+    end
+  case 'MSYS_NT*'
+    set -x OSTYPE windows
+    alias open explorer
 alias copy clip
-        alias paste 'powershell get-clipboard'
-        function to_win_path --argument path
-            if string match -r -q '^/[a-z]/' $path
-                set drive (string sub -s 2 -l 1 $path | string upper)
-                set rest (string sub -s 4 $path)
-                set p "$drive:/$rest"
-            end
-            echo $p
-        end
-        function trash --description "Move files or folders to Windows Recycle Bin"
-            for item in $argv
-                set winpath (to_win_path (realpath $item))
-                set cmd (test -d "$item"; and echo DeleteDirectory; or echo DeleteFile)
-                powershell -NoProfile -Command "Add-Type -AssemblyName Microsoft.VisualBasic;[Microsoft.VisualBasic.FileIO.FileSystem]::$cmd(\"$winpath\", 'OnlyErrorDialogs', 'SendToRecycleBin')"
-            end
-        end
-    case '*'
-        set -x OSTYPE unknown
+    alias paste 'powershell get-clipboard'
+    function to_win_path --argument path
+      if string match -r -q '^/[a-z]/' $path
+        set drive (string sub -s 2 -l 1 $path | string upper)
+        set rest (string sub -s 4 $path)
+        set p "$drive:/$rest"
+      end
+      echo $p
+    end
+    function trash --description "Move files or folders to Windows Recycle Bin"
+      for item in $argv
+        set winpath (to_win_path (realpath $item))
+        set cmd (test -d "$item"; and echo DeleteDirectory; or echo DeleteFile)
+        powershell -NoProfile -Command "Add-Type -AssemblyName Microsoft.VisualBasic;[Microsoft.VisualBasic.FileIO.FileSystem]::$cmd(\"$winpath\", 'OnlyErrorDialogs', 'SendToRecycleBin')"
+      end
+    end
+  case '*'
+    set -x OSTYPE unknown
 end
