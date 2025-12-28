09aba8821215970f454d55156201bd3d42a43ba6
CWorld <cworld0@qq.com>
2025-12-28 11:00:34 +0800

    feat(fish): :sparkles: update comments & enhance trash functions

--- config/fish/customize.fish
+++ config/fish/customize.fish
@@ -1,3 +1,4 @@
+# [Basic]
 # Prompt frefix
 set prompt_prefix "" # command logo
 # "": macOS     "": Windows  "": Android
@@ -7,10 +8,10 @@ set prompt_prefix "" # command logo
 # Proxy
 set proxy "http://127.0.0.1:7890"
 # set proxy $(cat /etc/resolv.conf | grep -oP '(?<=nameserver\ ).*'):7890 # for wsl
-
 # Fish enable vi mode
 source $fish_confs/vi_mode.fish
 
+# [Confs]
 # Package manager
 # source $fish_confs/pacman.fish
 # source $fish_confs/scoop.fish
@@ -34,15 +35,43 @@ source $fish_confs/nvim.fish
 # source $fish_confs/podman.fish
 # source $fish_confs/yazi.fish
 # source $fish_confs/zoxide.fish
+# source $fish_confs/jj.fish
 
+# [Others] sys detect
 switch (uname)
     case Linux
         set -x OSTYPE linux
+alias copy xclip
+        alias trash 'gio trash'
     case Darwin
         set -x OSTYPE macos
+alias copy pbcopy
+        function trash -d "Move files to trash on macOS"
+            for arg in $argv
+                set -l file (realpath "$arg")
+                /usr/bin/osascript -e "tell application \"Finder\" to delete POSIX file \"$file\"" >/dev/null
+            end
+        end
     case 'MSYS_NT*'
         set -x OSTYPE windows
         alias open explorer
+alias copy clip
+        alias paste 'powershell get-clipboard'
+        function to_win_path --argument path
+            if string match -r -q '^/[a-z]/' $path
+                set drive (string sub -s 2 -l 1 $path | string upper)
+                set rest (string sub -s 4 $path)
+                set p "$drive:/$rest"
+            end
+            echo $p
+        end
+        function trash --description "Move files or folders to Windows Recycle Bin"
+            for item in $argv
+                set winpath (to_win_path (realpath $item))
+                set cmd (test -d "$item"; and echo DeleteDirectory; or echo DeleteFile)
+                powershell -NoProfile -Command "Add-Type -AssemblyName Microsoft.VisualBasic;[Microsoft.VisualBasic.FileIO.FileSystem]::$cmd(\"$winpath\", 'OnlyErrorDialogs', 'SendToRecycleBin')"
+            end
+        end
     case '*'
         set -x OSTYPE unknown
 end
