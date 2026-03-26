eb99913292044fc25a3e931e9e9c033efc04b231
CWorld <cworld0@qq.com>
2026-03-26 17:24:18 +0800

    chore(fish): update script path

--- config/fish/conf.d/paths.fish
+++ config/fish/conf.d/paths.fish
@@ -1,5 +1,5 @@
 contains $HOME/.local/bin
 or set PATH $HOME/.local/bin $PATH
 
-contains $HOME/Documents/others/scripts
-or set PATH $HOME/Documents/others/scripts $PATH
+contains $HOME/.local/scripts
+or set PATH $HOME/.local/scripts $PATH
