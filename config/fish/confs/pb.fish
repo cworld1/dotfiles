a7f689ec1f4ee95363a2b1f15085d4bf5bb7e393
CWorld <cworld0@qq.com>
2026-06-17 00:01:08 +0800

    feat(fish): improve pb abbr

--- config/fish/confs/pb.fish
+++ config/fish/confs/pb.fish
@@ -1,2 +1,8 @@
 # See https://fars.ee/
-alias pb 'curl -F "c=@-" "http://fars.ee/"'
+abbr -a pb 'curl -F "c=@-" "http://fars.ee/"'
+abbr -a pb24 'curl -F sunset=1440 -F "c=@-" "http://fars.ee/"'
+abbr -a pbp24 'curl -F p=1 -F sunset=1440 -F "c=@-" "http://fars.ee/"'
+function pbd
+  echo "Excuting: curl -X DELETE \"http://fars.ee/$argv[1]\""
+  curl -X DELETE "http://fars.ee/$argv[1]"
+end
