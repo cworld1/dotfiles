f6d921e88c5fc65b3d4f7e9e9636a29ca5daa036
CWorld <cworld0@qq.com>
2026-07-24 23:53:12 +0800

    style(fish): format code

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,6 @@
-if status is-interactive
-  set fish_confs "$XDG_CONFIG_HOME"/fish/confs
-  source ~/.config/fish/customize.fish
+set fish_confs "$XDG_CONFIG_HOME"/fish/confs
+source ~/.config/fish/customize.fish
 
-  if [ -f "$XDG_CONFIG_HOME"/fish/private.fish ]
-      source "$XDG_CONFIG_HOME"/fish/private.fish
-  end
+if [ -f "$XDG_CONFIG_HOME"/fish/private.fish ]
+  source "$XDG_CONFIG_HOME"/fish/private.fish
 end
