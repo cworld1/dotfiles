0440fd666e8fb75ef9fe09a80899c9fef2614104
CWorld <cworld0@qq.com>
2026-09-06 13:57:57 +0800

    feat(fish): new cmake abbrs

--- config/fish/confs/cmake.fish
+++ config/fish/confs/cmake.fish
@@ -1,2 +1,4 @@
-abbr -a cmg 'cmake . -G "Unix Makefiles" -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1'
-abbr -a cmr 'cmake --build build && build/main'
+abbr -a cmkg 'cmake -S . -B build -G "Unix Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON'
+abbr -a cmkb 'cmake --build build'
+abbr -a cmkr 'cmake --build build; and ./build/main'
+abbr -a cmkc 'test -f CMakeLists.txt; and rm -rf build .cache'
