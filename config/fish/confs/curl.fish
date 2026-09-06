f2919957886e596aff4821d6e8cbf045e7a40604
CWorld <cworld0@qq.com>
2026-09-06 13:31:25 +0800

    feat(fish): support quick add new repo

--- config/fish/confs/curl.fish
+++ config/fish/confs/curl.fish
@@ -8,7 +8,26 @@ function pbd
   curl -X DELETE "http://fars.ee/$argv[1]"
 end
 
-# ip
+# IP
 abbr -a ip 'curl "https://myip.ipip.net/"'
 abbr -a ipv6 'curl -6 "https://myip.ipip.net/"'
 abbr -a ipv4 'curl -4 "https://myip.ipip.net/"'
+
+# GitHub
+function gh_newrepo
+  if test (count $argv) -lt 1; or test (count $argv) -gt 2
+    echo "Usage: gh_newrepo <repository-name> [-p]"
+    return 1
+  end
+  set -l repo $argv[1]
+  set -l private (test "$argv[2]" = "-p"; and echo true; or echo false)
+
+  read -sP "GitHub token: " token
+  echo "Requesting..."
+  curl -sS \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $token" \
+    -H "X-GitHub-Api-Version: 2026-03-10" \
+    -d "{\"name\":\"$repo\",\"private\":$private}" \
+    https://api.github.com/user/repos
+end
