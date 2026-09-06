d1aa0eda6d9615fb8051952e6567ba2b3417f690
CWorld <cworld0@qq.com>
2026-09-06 15:26:24 +0800

    feat(yazi): improve yazi settings & themes

--- config/fish/confs/yazi.fish
+++ config/fish/confs/yazi.fish
@@ -1,10 +1,15 @@
+export YAZI_CONFIG_HOME="$XDG_CONFIG_HOME"/yazi
+
+status is-interactive; or return
+
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+  set tmp (mktemp -t yazi-cwd.XXXXXX)
+  command yazi $argv --cwd-file=$tmp
+  if read -z cwd < $tmp
+    type -q cygpath; and set cwd (cygpath -u -- $cwd)
+    test "$cwd" != "$PWD"; and test -d "$cwd"; and cd -- "$cwd"
+  end
+  rm -f -- $tmp
 end
 
 complete -c yazi -l cwd-file -d 'Write the cwd on exit to this file' -r -F
@@ -13,5 +18,3 @@ complete -c yazi -l clear-cache -d 'Clear the cache directory'
 complete -c yazi -l debug -d 'Print debug information'
 complete -c yazi -s V -l version -d 'Print version'
 complete -c yazi -s h -l help -d 'Print help'
-
-export YAZI_CONFIG_HOME="$XDG_CONFIG_HOME"/yazi
