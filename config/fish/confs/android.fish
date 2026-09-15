be42a19c20f0195ec5d4ab923792f0382b30b4ac
CWorld <cworld0@qq.com>
2026-09-15 14:13:10 +0800

    fix(fish): fix android cmd for new version of fish

--- config/fish/confs/android.fish
+++ config/fish/confs/android.fish
@@ -1,2 +1,5 @@
-# alias adb "HOME=\"$XDG_DATA_HOME\"/android adb"
 set -gx ANDROID_USER_HOME "$XDG_DATA_HOME"/android
+# alias adb "HOME=$XDG_DATA_HOME/android adb"
+function adb
+    HOME="$XDG_DATA_HOME/android" command adb $argv
+end
